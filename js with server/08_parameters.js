e5d21f91601ec3007166e91073f8122fe51f376e
yunngna <nayunkwon124@gmail.com>
2024-11-11 17:22:36 +0900

    수정

--- js with server/08_parameters.js
+++ js with server/08_parameters.js
@@ -1,4 +1,4 @@
-//08_parameters.js
+
 //1) Default parameter 
 function say(message){
     if (message != undefined){
@@ -18,4 +18,17 @@ function printMsg(message='매개변수가 넘어오지 않습니다.'){
 printMsg('Hello!');
 printMsg();
 
-// 2) Rest parameter
+// 2) Rest parameter : 함수를 기반으로 실행 시 몇개의 parameter을 사용할지 모를 때 사용 | 계산을 기반으로 하는 처리에 자주 사용
+// 마지막부분의 매개변수 부분 ...rests (추가적으로 들어올지 안올지 모르는 경우) 
+const plus = function(x, y, ...rests){ 
+    let result = x + y;
+
+    for (let rest of rests){
+        result += rest;
+    }
+    return result;
+}
+
+console.log(plus(1,2)); // 3 
+console.log(plus(1,2,3,4,5)); // 15 
+console.log(plus(1,2,3,4,5,6,7,8,9,10)); // 55
