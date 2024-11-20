23a1b1ac65c68a819f18e3bf8ca29184ad92388d
yunngna <nayunkwon124@gmail.com>
2024-11-20 17:52:35 +0900

    1120 header

--- vue/mysql-project/src/App.vue
+++ vue/mysql-project/src/App.vue
@@ -4,15 +4,16 @@
     <!-- # header component-->
 
     <!--root 에 연결된 기본 라우터를 적어준다.-->
-    <router-link to="/customerList">전체조회 | </router-link>
-    <router-link to="/customerAdd">등록</router-link>
+    <!-- <router-link to="/customerList">전체조회 | </router-link>
+    <router-link to="/customerAdd">등록</router-link> -->
 
     <!-- # content component-->
 
     <!-- 직접 component 연결 (1) -->
     <!-- <CustomerInfo/> -->
     <!-- <CustomerList/> -->
-  
+    <DefaultHeader/>
+
     <!-- router  통해서 component 연결 (2)-->
     <router-view/>
 
@@ -28,11 +29,15 @@
 // import CustomerList from './views/CustomerList.vue';
 // import CustomerInfo from './views/CustomerInfo.vue';
 
-// export default{
-//   components : {
-//     CustomerList,
-//     CustomerInfo
-//   }
-// }
+import DefaultHeader from './layout/DefaultHeader.vue';
+
+
+export default{
+  components : {
+    //CustomerList,
+    //CustomerInfo,
+    DefaultHeader
+  }
+}
 
 </script>
---
+++ vue/mysql-project/src/layout/DefaultHeader.vue
@@ -0,0 +1,9 @@
+<!--layout/DefualtHeader.vue-->
+<!-- app.vue(대문) 자식 컴퍼넌트로 등록 -->
+<template>
+    <nav class="nav">
+    <!--router 연결 링크-->
+    <router-link class="nav-link" to="/customerList">전체조회</router-link>
+    <router-link class="nav-link" to="/customerAdd">등록</router-link>
+    </nav>
+</template>
